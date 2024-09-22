428dd53c02729bcb5e9ac313b8439cb28ff6558f
IQ-Vikash <vikash.kumar@indoqubix.com>
2024-09-22 10:31:25 +0530

    GitLab Code

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Nature from "./components/Project-4/Nature";
+// import UseEffect from "./components/Project-2/UseEffect";
+// import UseReducer from "./components/Project-2/UseReducer";
+// import UseStat from "./components/Project-2/UseStat"
+// import Todo from "./components/Project-3/Todo"
+
+function App() {
+  return (
+   <>
+   {/* <UseStat/>
+   <UseEffect/>
+   <UseReducer/> */}
+   {/* <Todo/> */}
+   <Nature/>
+   </>
+  );
+}
+
+export default App;
